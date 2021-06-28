3609bd6239decd9b9f8505ba2df6214cd9e45adb
Shaif Azad <shaif.rahi@northsouth.edu>
2021-06-28 13:53:54 +0600

    port changed

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function (req,res) {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8100;
+const PORT = process.env.PORT || 8101;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
